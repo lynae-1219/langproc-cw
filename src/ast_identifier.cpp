f82faf2134b7402191244aac80a7750f267e3791
Kevin <kevincywong@gmail.com>
2025-08-05 14:51:19 +0000

    Basic QOL Refinements, this is it for a bit

--- src/ast_identifier.cpp
+++ src/ast_identifier.cpp
@@ -5,21 +5,18 @@ namespace ast {
 Identifier::Identifier(std::string identifier) 
     : identifier_(std::move(identifier)) {}
 
-// ADDED: New implementation for GetType
 Context::Type Identifier::GetType(Context& context) const {
     return context.GetVariableType(identifier_);
 }
 
-// AMENDED: Emits different code based on variable type.
+
 void Identifier::EmitRISC(std::ostream& stream, Context& context) const {
     int offset = context.GetVariableOffset(identifier_);
-    Context::Type type = this->GetType(context); // Use our new method
+    Context::Type type = this->GetType(context);
 
     if (type == Context::Type::INT) {
-        // Load an integer variable into an integer register (a0)
         stream << "  lw a0, " << offset << "(sp)" << std::endl;
     } else if (type == Context::Type::FLOAT) {
-        // Load a float variable into a float register (fa0)
         stream << "  flw fa0, " << offset << "(sp)" << std::endl;
     }
 }
